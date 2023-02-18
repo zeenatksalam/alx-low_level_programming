88b5367b0c4e2073bd375bedc1b483ef3230ddba
root <zeenatksalam@gmail.com>
2023-02-18 07:46:13 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#iclude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints "with proper grammar, but the outcome is 
@@ -11,4 +11,5 @@ int main(void)
 
 {
         printf("with proper grammer, but the outcome is a piece of art,\n");
-        return (0);	
+        return (0);
+}	
