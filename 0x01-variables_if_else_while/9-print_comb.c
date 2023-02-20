43f31167a3a36ef88b5963b632aa18ef48871518
root <zeenatksalam@gmail.com>
2023-02-20 01:23:03 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,9 @@ int main(void)
 		putchar(n);
 		if (n != 57)
 		{
-
-		       putchar(',');
-		       putchar(' ');
-		   }
-	putchar('\n');
-	return (0);
+			putchar(',');
+				putchar(' ');
+		}
+		putchar('\n');
+		return (0);
 }
