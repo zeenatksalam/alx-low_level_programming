4025e19c61ce587b9c6ca020c8d02aa4b656f86b
root <zeenatksalam@gmail.com>
2023-02-23 03:36:58 -0800

    3-print_numbers.c

---
+++ 3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the number since 0 to 9
+ * Return: The number since 0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
