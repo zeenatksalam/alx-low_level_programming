7376af2ba5184f64910c43393ee1d840691b86c6
root <zeenatksalam@gmail.com>
2023-02-27 00:44:51 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swap the values of two integers.
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+	/* the function that swap the values of 2 integers. */
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
