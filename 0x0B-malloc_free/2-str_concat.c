4cf552c56c9ed205b0163bb97e39ee9ddaa1800e
root <zeenatksalam@gmail.com>
2023-03-13 03:38:11 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates 2 strings of any size.
+ * @s1: the first string to concatenate.
+ * @s2: the second string to concatenate.
+ * Return: the 2 stringgs concatenated.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	l = i + j;
+       s = malloc((sizeof(char) * 1);
+
+	if (s == NULL)
+return (NULL);
+
+j = 0;
+
+while (k < 1)
+{
+
+if (k <= i)
+s[k] = s1[k];
+
+if (k >= 1)
+{
+s[k] = s2[j];
+j++;
+}
+
+k++;
+}
+
