a8113d0ef62a01c83f4b37bdccbcdee531575ad0
root <zeenatksalam@gmail.com>
2023-03-20 07:16:02 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+struct dog
+
+int _putchar.c(char c);
