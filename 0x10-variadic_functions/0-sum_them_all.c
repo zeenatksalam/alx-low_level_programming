e040e2126a304246a562df5222da8b47cf529ecb
root <zeenatksalam@gmail.com>
2023-03-23 04:50:01 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,8 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if (n == 0)
-		return (0);
+	if (i == 0)
+	return (0);
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
